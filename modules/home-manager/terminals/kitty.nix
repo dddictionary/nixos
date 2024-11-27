406bb8dd9b59ff72a3a84834c5382f1a24074142
Abrar Habib <abrarhabib285@gmail.com>
2024-11-26 23:54:43 -0500

    Changed font to IBM Plex Mono

--- modules/home-manager/terminals/kitty.nix
+++ modules/home-manager/terminals/kitty.nix
@@ -1,19 +1,24 @@
+{ pkgs, ... }:
+
 {
-	programs.kitty = {
-		enable = true;
-		theme = "Gruvbox Dark Hard";
-		font.name = "Noto";
-		shellIntegration.enableZshIntegration = true;
-		settings = {
-			font_family = "Noto Sans Mono Thin";
-			font_size = 14;
-			background_opacity = "0.85";
-			cursor_shape = "beam";
-			open_url_with = "firefox";
-			detect_urls = true;
-			remember_window_size = true;
-			initial_window_width = 480;
-			initial_window_height = 270;
-		};
-	};
+  programs.kitty = {
+    enable = true;
+    theme = "Gruvbox Dark Hard";
+		font.name = "IBM Plex Mono";
+    font.package = pkgs.ibm-plex;
+    font.size = 13.5;
+    shellIntegration.enableZshIntegration = true;
+    settings = {
+      # font_family = "IBM Plex Mono Thin";
+      # font_size = 13;
+      background_opacity = "0.95";
+      cursor_shape = "beam";
+      open_url_with = "firefox";
+      detect_urls = true;
+      dynamic_background_opacity = true;
+      remember_window_size = true;
+      initial_window_width = 480;
+      initial_window_height = 270;
+    };
+  };
 }
